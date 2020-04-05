66795e8c638f2460de8bc291a10310081218a080
Koichi-hub <61937439+Koichi-hub@users.noreply.github.com>
2020-04-05 22:35:38 +0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,12 +1,11 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
-import datetime
-import random
 import wikipedia
 import googletrans
 import os
 import requests
+
 wikipedia.set_lang("RU")
 
 token_lol = os.environ.get('token_bot_bot')
@@ -21,14 +20,11 @@ longpoll = VkBotLongPoll(vk, group_id=group_lol)
 
 print('Bot start')
 
-who = ['никем', 'рабочим с завода', 'сотрудником макдональдса', 'офисным клерком']
-act = ['все останеться как есть', 'жизнь измениться к лучшему', 'жизнь измениться к худшему']
-
 
 def write_msg(peer_id, message):
     vk.method('messages.send', {'peer_id': peer_id, 'message': message, 'random_id': get_random_id()})
-    
-    
+
+
 def per(peer_id, message):
     write_msg(peer_id, googletrans.Translator().translate(message, dest='ru').text)
 
@@ -37,48 +33,27 @@ while True:
     try:
         for event in longpoll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW and event.object.peer_id != event.object.from_id:
-                
+
                 if event.obj.text == '.перевод':
                     write_msg(event.obj.peer_id, 'Введите текст)')
                     for event in longpoll.listen():
                         if event.type == VkBotEventType.MESSAGE_NEW and event.object.peer_id != event.object.from_id:
                             per(event.obj.peer_id, event.obj.text)
                             break
-                
+
                 if event.obj.text == '.погода':
                     pog = os.environ.get('pog')
                     response = requests.get(str(pog))
                     response = response.json()
-                    b = [response['name'], response['sys']['country'], response['weather'][0]['main'], int(response['main']['temp'])-273, response['wind']['speed'], datetime.datetime.today().strftime("%H:%M:%S")]
+                    b = [response['name'], response['sys']['country'], response['weather'][0]['main'],
+                         int(response['main']['temp']) - 273, response['wind']['speed']]
 
-                    write_msg(event.obj.peer_id, f'В городе {b[0]}, {b[1]}, время - {b[5]}, сейчас - {b[2]}, температура - {b[3]}, скорость ветра - {b[4]}')
+                    write_msg(event.obj.peer_id,
+                              f'В городе {b[0]}, {b[1]}, сейчас - {b[2]}, температура - {b[3]}, скорость ветра - {b[4]}')
 
                 if event.obj.text.lower() == '.команды':
-                    write_msg(event.obj.peer_id, '.время, .погода, .перевод, .яинфа, .нас, .будущее, .вики, .чат, .инфа')
-
-                if event.object.text.lower() == '.время':
-                    write_msg(event.object.peer_id, 'сегодня ' + str(datetime.date.today()) + ' сейчас ' +
-                              str(datetime.datetime.today().strftime("%H:%M:%S")))
-
-                if event.object.text.lower() == '.яинфа':
-                    info = vk.method('users.get', {'user_ids': event.object.from_id, 'fields': 'sex'})[0]
-                    info_1 = ''
-                    for key, value in info.items():
-                        if key == 'sex':
-                            if value == 1:
-                                value = 'женский'
-                            if value == 2:
-                                value = 'мужской'
-                            else:
-                                value = 'не указан'
-                            info_1 += '{0}:{1}\n'.format(key, value)
-                        else:
-                            info_1 += '{0}:{1}\n'.format(key, value)
-                    write_msg(event.object.peer_id, 'информация\n' + info_1)
-
-                if event.object.text.lower() == '.нас':
-                    members = vk.method('messages.getConversationMembers', {'peer_id': event.obj.peer_id, 'fields': 'count'})
-                    write_msg(event.object.peer_id, 'количество участников ' + str(members['count']))
+                    write_msg(event.obj.peer_id,
+                              '.погода, .перевод, .вики, .чат,')
 
                 if event.object.text.lower() == '.чат':
                     if event.obj.from_id == 379076419:
@@ -98,21 +73,6 @@ while True:
                     else:
                         write_msg(event.obj.peer_id, 'у тебя нет доступа')
 
-                if event.obj.text.lower() == '.будущее':
-                    write_msg(event.obj.peer_id, 'Введите имя')
-                    for event in longpoll.listen():
-                        if event.type == VkBotEventType.MESSAGE_NEW and event.object.peer_id != event.object.from_id:
-                            if len(event.obj.text) > 50:
-                                write_msg(event.obj.peer_id, 'уменьши сообщение)')
-                                break
-                            if event.obj.text == 'тимур' or event.obj.text == 'Тимур':
-                                write_msg(event.obj.peer_id, 'информация вам недоступна')
-                                break
-                            else:
-                                write_msg(event.obj.peer_id,
-                                          '{0} вы станете {1} и {2}. А вообще, кто знает...'.format(event.obj.text, random.choice(who), random.choice(act)))
-                                break
-
                 if event.obj.text.lower() == '.вики':
                     write_msg(event.obj.peer_id, 'введите запрос')
                     for event in longpoll.listen():
@@ -121,33 +81,12 @@ while True:
                                 write_msg(event.obj.peer_id, 'уменьши сообщение)')
                                 break
                             try:
-                                write_msg(event.obj.peer_id, 'Вот что я нашел: \n' + str(wikipedia.summary(event.obj.text)[:500]))
+                                write_msg(event.obj.peer_id,
+                                          'Вот что я нашел: \n' + str(wikipedia.summary(event.obj.text)[:500]))
                                 break
                             except wikipedia.exceptions.DisambiguationError:
                                 write_msg(event.obj.peer_id, 'запрос не выполнен')
                                 break
-                                
-                if event.obj.text == '.инфа':
-                    write_msg(event.obj.peer_id, 'id:')
-                    for event in longpoll.listen():
-                        if event.type == VkBotEventType.MESSAGE_NEW and event.object.peer_id != event.object.from_id:
-                            if event.obj.from_id == 379076419:
-                                info = vk.method('users.get', {'user_ids': int(event.obj.text), 'fields': 'sex'})[0]
-                                info_1 = ''
-                                for key, value in info.items():
-                                    if key == 'sex':
-                                        if value == 1:
-                                            value = 'женский'
-                                        if value == 2:
-                                            value = 'мужской'
-                                        else:
-                                            value = 'не указан'
-                                        info_1 += '{0}:{1}\n'.format(key, value)
-                                    else:
-                                        info_1 += '{0}:{1}\n'.format(key, value)
-                                write_msg(event.object.peer_id, 'информация\n' + info_1)
-                            else:
-                                break 
 
     except Exception as E:
         print(Exception)
